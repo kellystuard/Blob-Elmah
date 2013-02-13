6269fe77a40f801fc825d1ed3dd19fc5de96d6de
kellystuard <kellystuard@gmail.com>
2013-02-13 16:20:39 -0600

    Update BlobErrorLog.cs
    * Make work with Azure Storage 2.0.
    * Blob name now also error id.
    * Blob list pre-sorted, with newest first.
    * GetError can now call for direct blob, instead of first getting list.
    * No additional call needed, in loop of list, as casting blob as appropriate type.
    * Substantial decrease in Azure transactions, due to more targeted calls.
    * Blob container no longer public, by default (this security hole allowed anonymous browsing of error information).

--- BlobErrorLog.cs
+++ BlobErrorLog.cs
@@ -1,135 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.Collections;
+using System.Globalization;
 using System.Linq;
 using System.Text;
-using Elmah;
-using System.Collections;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.StorageClient;
-using System.Web.Configuration;
 using System.Xml;
-using System.IO;
-using System.Globalization;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Elmah.Azure
 {
-    public class BlobErrorLog : ErrorLog
+    public sealed class BlobErrorLog : ErrorLog
     {
-        private string connectionStringName = null;
-
         public BlobErrorLog(IDictionary config)
         {
+            if (config == null) throw new ArgumentNullException("config");
             if (!config.Contains("connectionStringName"))
                 throw new System.ApplicationException("Configuration string is missing for the Windows Azure Blob Storage Error Log.");
 
-            this.connectionStringName = (string)config["connectionStringName"];
+            _connectionString = RoleEnvironment.GetConfigurationSettingValue((string)config["connectionStringName"]);
+            Initialize();
+        }
+
+        public BlobErrorLog(string connectionString)
+        {
+            if (connectionString == null) throw new ArgumentNullException("connectionString");
+
+            this._connectionString = connectionString;
             this.Initialize();
-        }
-
-        public BlobErrorLog(string connectionStringName)
-        {
-            this.connectionStringName = connectionStringName;
-            this.Initialize();
-        }
-
-        private void Initialize()
-        {
-            var blobContainer = GetBlobContainer();
-            blobContainer.CreateIfNotExist();
-            blobContainer.SetPermissions(new BlobContainerPermissions() { PublicAccess = BlobContainerPublicAccessType.Blob });
-        }
-
-        private CloudBlobContainer GetBlobContainer()
-        {
-            var blobClient = GetStorageAccount().CreateCloudBlobClient();
-            var blobContainer = blobClient.GetContainerReference("elmaherrors");
-            return blobContainer;
-        }
-
-        private CloudStorageAccount GetStorageAccount()
-        {
-            var connectionString = WebConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
-            return CloudStorageAccount.Parse(connectionString);
         }
 
         public override ErrorLogEntry GetError(string id)
         {
-            ErrorLogEntry entry;
-            try
-            {
-                id = new Guid(id).ToString();
-            }
-            catch (FormatException exception)
-            {
-                throw new ArgumentException(exception.Message, id, exception);
-            }
+            if (id == null) throw new ArgumentNullException("id");
 
             var blobContainer = GetBlobContainer();
-            var blobs = blobContainer.ListBlobs();
+            var blob = blobContainer.GetBlockBlobReference(id);
 
-            var blobItem = blobs.FirstOrDefault(b => b.Uri.ToString().EndsWith(id + ".xml"));
-            if (blobItem == null)
-                throw new FileNotFoundException(string.Format("Cannot locate error file for error with ID {0}.", id));
-
-            using (var stream = new MemoryStream())
+            using (var blobStream = blob.OpenRead())
+            using (var reader = new XmlTextReader(blobStream))
             {
-                var blob = blobContainer.GetBlobReference(blobItem.Uri.ToString());
-                blob.DownloadToStream(stream);
-                stream.Position = 0;
-                using (var reader = new XmlTextReader(stream))
-                {
-                    Error error = ErrorXml.Decode(reader);
-                    entry = new ErrorLogEntry(this, id, error);
-                }
+                var error = ErrorXml.Decode(reader);
+                return new ErrorLogEntry(this, id, error);
             }
-            return entry;
         }
 
         public override int GetErrors(int pageIndex, int pageSize, IList errorEntryList)
         {
-            if (pageIndex < 0)
-                throw new ArgumentOutOfRangeException("pageIndex", pageIndex, null);
-
-            if (pageSize < 0)
-                throw new ArgumentOutOfRangeException("pageSize", pageSize, null);
+            if (pageIndex < 0) throw new ArgumentOutOfRangeException("pageIndex", pageIndex, null);
+            if (pageSize < 0) throw new ArgumentOutOfRangeException("pageSize", pageSize, null);
+            if (errorEntryList == null) throw new ArgumentNullException("errorEntryList");
 
             var blobContainer = GetBlobContainer();
-            var blobs = blobContainer.ListBlobs();
-            if (blobs.Count() < 1)
-                return 0;
+            var blobs = blobContainer.ListBlobs()
+                .OfType<CloudBlockBlob>()
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
 
-            int count = 0;
-            string[] keys = new string[blobs.Count()];
-
+            var count = 0;
             foreach (var blob in blobs)
             {
-                keys[count++] = blob.Uri.ToString();
-            }
-
-            Array.Sort(keys, 0, count, Comparer.DefaultInvariant);
-            Array.Reverse(keys, 0, count);
-            
-            if (errorEntryList != null)
-            {
-                int firstIndex = pageIndex * pageSize;
-                int lastIndex = (firstIndex + pageSize < count) ? firstIndex + pageSize : count;
-                for (int i = firstIndex; i < lastIndex; i++)
+                count++;
+                using (var blobStream = blob.OpenRead())
+                using (var reader = new XmlTextReader(blobStream))
                 {
-                    var blob = blobContainer.GetBlobReference(keys[i]);
-                    using (var stream = new MemoryStream())
-                    {
-                        blob.DownloadToStream(stream);
-                        stream.Position = 0;
-                        using (XmlTextReader reader = new XmlTextReader(stream))
-                        {
-                            while (reader.IsStartElement("error"))
-                            {
-                                string attribute = reader.GetAttribute("errorId");
-                                Error error = ErrorXml.Decode(reader);
-                                errorEntryList.Add(new ErrorLogEntry(this, attribute, error));
-                            }
-                        }
-                    }
+                    var error = ErrorXml.Decode(reader);
+                    errorEntryList.Add(new ErrorLogEntry(this, blob.Name, error));
                 }
             }
             return count;
@@ -137,27 +73,20 @@ namespace Elmah.Azure
 
         public override string Log(Error error)
         {
-            string errorId = Guid.NewGuid().ToString();
-            DateTime time = (error.Time > DateTime.MinValue) ? error.Time : DateTime.Now;
-            string fileName = string.Format(CultureInfo.InvariantCulture, "error-{0:yyyy-MM-ddHHmmssZ}-{1}.xml", new object[] { time.ToUniversalTime(), errorId });
+            var errorId = string.Format(CultureInfo.InvariantCulture, "error-{0:x16}-{1:N}.xml",
+                (DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks),
+                Guid.NewGuid()
+            );
 
             var blobContainer = GetBlobContainer();
-            var blob = blobContainer.GetBlobReference(fileName);
+            var blob = blobContainer.GetBlockBlobReference(errorId);
 
-            using (var memory = new MemoryStream())
+            using (var blobStream = blob.OpenWrite())
+            using (var writer = new XmlTextWriter(blobStream, Encoding.UTF8))
             {
-                using (XmlTextWriter writer = new XmlTextWriter(memory, Encoding.UTF8))
-                {
-                    writer.Formatting = Formatting.Indented;
-                    writer.WriteStartElement("error");
-                    writer.WriteAttributeString("errorId", errorId);
-                    ErrorXml.Encode(error, writer);
-                    writer.WriteEndElement();
-                    writer.Flush();
-
-                    memory.Position = 0;
-                    blob.UploadFromStream(memory);
-                }
+                writer.WriteStartElement("error");
+                ErrorXml.Encode(error, writer);
+                writer.WriteEndElement();
             }
             return errorId;
         }
@@ -169,5 +98,26 @@ namespace Elmah.Azure
                 return "Windows Azure Blob Storage Error Log";
             }
         }
+
+        private void Initialize()
+        {
+            var blobContainer = GetBlobContainer();
+            blobContainer.CreateIfNotExists();
+        }
+
+        private CloudBlobContainer GetBlobContainer()
+        {
+            var blobClient = CloudStorageAccount.Parse(_connectionString).CreateCloudBlobClient();
+            var blobContainer = blobClient.GetContainerReference("elmaherrors");
+            return blobContainer;
+        }
+
+        private string NewLogId()
+        {
+            return (DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks).ToString("d19")
+                + '-' + Guid.NewGuid().ToString("n");
+        }
+
+        private readonly string _connectionString;
     }
 }
